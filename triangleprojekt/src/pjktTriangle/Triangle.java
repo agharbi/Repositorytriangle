b123b6cfcd2efa945780fb026341379acef57d5c
26saida <26saida@26saida-PC.lan>
2013-05-26 16:13:02 +0200

    commit Klasse "TriangleTest.java" wurde nochmals rennen lassen 	   Test ist Erfolgreich (Run finished after 0.012 seconds, Errors 0 	   und Failurs 0)

--- triangleprojekt/src/pjktTriangle/Triangle.java
+++ triangleprojekt/src/pjktTriangle/Triangle.java
@@ -8,20 +8,20 @@ import java.util.*;
 public class Triangle {
 	
 	//add Methode berechnet die Flaeche
-	public static double flaeche (double kathetea , double katheteb) {
+	public double flaeche (double kathetea , double katheteb) {
 	    double s = (kathetea * katheteb) / 2;
 	    return s;
     }
     
     //Methode berechnet die Hypotenuse
-    public static double hypotenuse (double kathetea , double katheteb) {
+    public double hypotenuse (double kathetea , double katheteb) {
      	double h = Math.sqrt(kathetea * kathetea + katheteb * katheteb);
         return h;
     }
     
   	//Methode berechnet der winkle, parameter alpha ist bekannt, 
     //ein rechtwinkliges Dreieck besitzt einen 90°
-    public static double winkel (double alpha) {
+    public double winkel (double alpha) {
  	    double w = 180 - 90 - alpha;
  	 	return w;
     }   
@@ -30,10 +30,12 @@ public class Triangle {
 		  Triangle t = new Triangle();
 		  double alpha = 50.10;
 		  double kathetea = 35.45;
-		  double katheteb = 35.00;	
-		  double f1 = flaeche (kathetea , katheteb);
-		  double h1 = hypotenuse (kathetea , katheteb);
-		  double w1 = winkel (alpha);
+		  double katheteb = 35.00;
+		  double f1 = t.flaeche (kathetea , katheteb);
+		  double h1 = t.hypotenuse (kathetea , katheteb);
+		  double w1 = t.winkel (alpha);
 		  System.out.println(t   + "(" + f1 + "," + h1 + "," + w1 + ")");
+		  System.out.println(t.flaeche(3 , 4));
+		  System.out.println(t.hypotenuse(3.0 , 4.0));
     }
 }
