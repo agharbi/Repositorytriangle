b1955d01663818e5084567e255834b2b49365a40
26saida <26saida@26saida-PC.lan>
2013-05-26 14:23:12 +0200

    commit Implementierung der "main" Methode und dann die Klasse        "Triangle"" wurde kompiliert und dann erfolgreich ausgeführt

--- triangleprojekt/src/pjktTriangle/Triangle.java
+++ triangleprojekt/src/pjktTriangle/Triangle.java
@@ -1,32 +1,39 @@
 package pjktTriangle;
 
-//Klasse Triangle erstellen
+import java.lang.Math;
+import java.util.*;
 
+
+//Klasse Triangle erstellen
 public class Triangle {
+	
 	//add Methode berechnet die Flaeche
 	public static double flaeche (double kathetea , double katheteb) {
 	    double s = (kathetea * katheteb) / 2;
 	    return s;
-}
+    }
     
     //Methode berechnet die Hypotenuse
     public static double hypotenuse (double kathetea , double katheteb) {
      	double h = Math.sqrt(kathetea * kathetea + katheteb * katheteb);
         return h;
-         }
+    }
     
   	//Methode berechnet der winkle, parameter alpha ist bekannt, 
     //ein rechtwinkliges Dreieck besitzt einen 90°
-     public static double winkel (double alpha) {
- 	 	    double w = 180 - 90 - alpha;
- 	 	    return w;
- 	     }   
+    public static double winkel (double alpha) {
+ 	    double w = 180 - 90 - alpha;
+ 	 	return w;
+    }   
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		Triangle triangle = new Triangle();
-
-}
+		  Triangle t = new Triangle();
+		  double alpha = 50.10;
+		  double kathetea = 35.45;
+		  double katheteb = 35.00;	
+		  double f1 = flaeche (kathetea , katheteb);
+		  double h1 = hypotenuse (kathetea , katheteb);
+		  double w1 = winkel (alpha);
+		  System.out.println(t   + "(" + f1 + "," + h1 + "," + w1 + ")");
+    }
 }
